d315f936ed7964228ee1f775f9bf2bc32df28467
Kevin Dinkel <Kevin.Dinkel@lasp.colorado.edu>
2023-04-17 15:04:54 -0600

    removing everything but base pico.ads

--- src/pico-pimoroni.ads
+++
@@ -1,8 +0,0 @@
---
---  Copyright 2021 (C) Jeremy Grosser <jeremy@synack.me>
---
---  SPDX-License-Identifier: BSD-3-Clause
---
-package Pico.Pimoroni is
-   --  Root package for all the Pimoroni peripherals
-end Pico.Pimoroni;
